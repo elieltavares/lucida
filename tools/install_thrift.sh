0ac3f85f2173d38693280ddb724213539a0bcfc4
elieltavares <elinkpark@gmail.com>
2016-09-03 14:08:02 -0300

    Removed $THREADS and add j1
    I removed the $THREADS and added j1 because I was catching my computer with $THREADS

--- tools/install_thrift.sh
+++ tools/install_thrift.sh
@@ -24,8 +24,8 @@ wget "http://archive.apache.org/dist/thrift/$THRIFT_VERSION/thrift-$THRIFT_VERSI
   && tar xf thrift-$THRIFT_VERSION.tar.gz \
   && cd thrift-$THRIFT_VERSION \
   && ./configure --with-lua=no --with-ruby=no --with-go=no --with-erlang=no --with-nodejs=no \
-  && make -j $THREADS\
-  && sudo make -j $THREADS install \
+  && make -j1\
+  && sudo make -j1 install \
   && cd lib/py/ \
   && sudo python setup.py install \
   && cd ../../lib/java/ \
